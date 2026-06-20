aec5197f2f21304c52985ebc88ac0c17b50cb21d
NAMAN ANAND <2024eb02504@online.bits-pilani.ac.in>
2026-06-20 23:03:26 +0530

    imporvise getAnalyticsDashboard function

--- backend/controllers/analyticsController.js
+++ backend/controllers/analyticsController.js
@@ -1,6 +1,5 @@
 import { getAnalytics } from "../services/analyticsService.js";
 
-export const getAnalyticsDashboard = async (_req, res) => {
-  const analytics = await getAnalytics();
-  res.json({ success: true, data: analytics });
+
+ 
 };
